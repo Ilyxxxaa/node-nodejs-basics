45003cdfe73db6ac7e1731d2d345715bb64842d1
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-06-19 02:40:26 +0300

    feat: end with cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+const PREFIX = '--';
+
 const parseArgs = () => {
   console.log(process.argv);
+  const args = process.argv.reduce((acc, value, index, array) => {
+    if (value.startsWith(PREFIX)) {
+      const string = `${value.replace(PREFIX, '')} is ${array[index + 1]}`;
+      return [...acc, string];
+    }
+    return acc;
+  }, []);
+
+  console.log(args.join(', '));
 };
 
 parseArgs();
