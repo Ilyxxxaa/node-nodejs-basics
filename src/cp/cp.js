0dcd2ac920358a061ebb9eb2511cb7fd39114ff6
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-06-23 00:13:47 +0300

    feat: end with cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
+const pathToScript = fileURLToPath(new URL('./files/script.js', import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', [pathToScript, ...args]);
+
+  stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  childProcess.stdout.on('data', (data) => {
+    stdout.write(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Thank', 'you', 'for', 'waiting']);
