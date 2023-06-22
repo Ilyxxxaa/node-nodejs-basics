3e66db118f212c85d7b6d1d618b375ea17fdae82
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-06-23 00:53:51 +0300

    feat: end with wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import os from 'os';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
+const pathToWorker = fileURLToPath(new URL('./worker.js', import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const arrayOfCPU = os.cpus();
+  let i = 10;
+
+  const workersArray = arrayOfCPU.map((_, index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(pathToWorker, { workerData: index + i });
+      worker.on('message', (data) => resolve({ status: 'resolved', data }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+  });
+
+  const result = await Promise.all(workersArray);
+  console.log(result);
 };
 
 await performCalculations();
