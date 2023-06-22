5f3bfff06ba9439169cdec72e41f1a81e577f3e1
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2023-06-22 23:37:51 +0300

    feat: end with streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const pathToFile = fileURLToPath(new URL('./files/fileToRead.txt', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(pathToFile);
+  readStream.pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { stdin } from 'process';
+import { pipeline } from 'stream/promises';
+
+const pathToFile = fileURLToPath(new URL('./files/fileToWrite.txt', import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(pathToFile, { flags: 'a' });
+  await pipeline(stdin, writeStream);
 };
 
 await write();
